fd9377b47623b16f4bc5599b8a6f175ab72cce30
Simon Barboza <simonbarboza@168.0.16.172.net-uno.net>
2023-03-17 20:36:20 -0400

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const About = () => {
+  return (
+    <>
+    <h1>About</h1>
+    <Link href={"/"}>Link to Home Page</Link>
+    </>
+  )
+}
+
+export default About
